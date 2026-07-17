87204610d1ea0cecbd11bfda4816558ed990c5cb
ustrungnguyen <contact@ngtrung.com>
2026-07-17 10:53:00 +0700

    Add GlobalExceptionHandler

---
+++ server/src/main/java/com/pleasebookme/server/global/handler/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.pleasebookme.server.global.handler;
+
+import com.pleasebookme.server.global.exception.ResourceNotFoundException;
+import com.pleasebookme.server.global.response.ApiErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleResourceNotFoundException(
+        ResourceNotFoundException exception,
+        HttpServletRequest request
+    ) {
+        ApiErrorResponse error = new ApiErrorResponse(
+            "error",
+            exception.getMessage(),
+            null,
+            request.getRemoteAddr(),
+            request.getRequestURI()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
